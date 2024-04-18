98b850334fd20206519f5e80f207f509a3fab7f6
Ridhwankiki <ridhwanr0607@gmail.com>
2024-04-18 11:35:03 +0700

    Initial Commit

---
+++ Montra/MontraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MontraApp.swift
+//  Montra
+//
+//  Created by MacBook Pro on 18/04/24.
+//
+
+import SwiftUI
+
+@main
+struct MontraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
